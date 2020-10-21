1d98e8b2de64a66567b1de3c7467ce003aa2e653
Rick Nitsche <rick@phas.ubc.ca>
2020-10-20 21:22:41 -0700

    fix(report): fix /signoff report

--- src/report.js
+++ src/report.js
@@ -17,7 +17,7 @@ const sendConfirmation = async(report) => {
 
   text += report.system_health_ok ? report.system_health_note ?
     'System Health ' : '' :
-    'There is an issue on the System Health Quicklook panel.\n';
+    'There are issues on the System Health Quicklook panel.\n';
   text += report.system_health_note ? `Note: ${report.system_health_note}\n\n` :
     report.system_health_ok ? '' : '\n';
 
@@ -42,7 +42,7 @@ const sendConfirmation = async(report) => {
   text += report.transit_flux_note ? `Note: ${report.transit_flux_note}\n\n` :
     report.transit_flux ? '\n' : '';
 
-  text += report.ringmap_ok ? report.ringmap_note ? 'Ringmap: ' : '' :
+  text += report.ringmap_ok ? report.ringmap_note ? 'Ringmap ' : '' :
     'There are issues with the ringmap.\n';
   text += report.ringmap_note ? `Note: ${report.ringmap_note}\n\n` :
     report.ringmap_ok ? '' : '\n';
@@ -50,7 +50,7 @@ const sendConfirmation = async(report) => {
   let message = payloads.confirmation({
     channel_id: process.env.ANASTASIA_SLACK_CHANNEL,
     report: text,
-    user: report.user_name,
+    user: report.user_id,
   });
 
   let result = await api.callAPIMethod('chat.postMessage', message);
@@ -62,21 +62,23 @@ const sendConfirmation = async(report) => {
 const create = async(userId, view) => {
   let values = view.state.values;
 
-  let result = await api.callAPIMethod('users.info', {
-    user: userId,
-  });
+  let system_health_selection =
+    values.system_health_block.system_health_ok.selected_options;
+  let alerts_selection = values.alerts_block.alerts_ok.selected_options;
+  let ringmap_selection = values.ringmap_block.ringmap_ok.selected_options;
 
   await sendConfirmation({
-    userId,
-    user_name: result.user.name,
+    user_id: userId,
     num_nodes: values.num_nodes_block.num_nodes.value,
     num_nodes_note: values.num_nodes_note_block.num_nodes_note.value,
     median_rfi: values.median_rfi_block.rfi.value,
     median_rfi_note: values.median_rfi_note_block.median_rfi_note.value,
-    system_health_ok: values.system_health_block.system_health_ok.value,
+    system_health_ok: (system_health_selection !== undefined
+      && system_health_selection.length === 1),
     system_health_note:
       values.system_health_note_block.system_health_note.value,
-    alerts_ok: values.alerts_block.alerts_ok.value,
+    alerts_ok: (alerts_selection !== undefined
+      && alerts_selection.length === 1),
     alerts_note: values.alerts_note_block.alerts_note.value,
     calibration: values.calibration_block.calibration.value,
     calibration_note: values.calibration_note_block.calibration_note.value,
@@ -86,7 +88,8 @@ const create = async(userId, view) => {
     gains_note: values.gains_note_block.gains_note.value,
     transit_flux: values.transit_block.transit_flux.value,
     transit_flux_note: values.transit_note_block.transit_note.value,
-    ringmap_ok: values.ringmap_block.ringmap_ok.value,
+    ringmap_ok: (ringmap_selection !== undefined
+      && ringmap_selection.length === 1),
     ringmap_note: values.ringmap_note_block.ringmap_note.value,
   });
 };
